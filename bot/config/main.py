5e9ca7f67fcc0e7e06a62cfbd510a747959b99e5
Anton Zimin <zimkaa87@gmail.com>
2023-10-17 06:34:12 +0300

    new version
    Signed-off-by: Anton Zimin <zimkaa87@gmail.com>

--- bot/config/main.py
+++ bot/config/main.py
@@ -11,10 +11,10 @@ pydantic.BaseSettings = BaseSettings
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(extra="ignore", env_file=".env", env_file_encoding="utf-8")
 
-    app_version: str = "0.1.0"
+    app_version: str = "0.7.0"
 
     # Project settings:
-    project_name: str = "SalesBot"
+    project_name: str = "sales_bot"
 
     # development / test / production
     environment: str = "development"
@@ -22,7 +22,7 @@ class Settings(BaseSettings):
     logger: logging.Logger = logging.getLogger("SalesLogger")
 
     # Telegram settings:
-    telegram_admin_id: int = 5218682536
+    telegram_admin_id: int = 489534969
     telegram_bot_name: str = "default_bot"
     telegram_bot_token: str = "default_token"
     telegram_api_id: int = 0000000000
