08a976e2621a1444dfc19106b7443bb699c645d6
Anton Zimin <zimkaa87@gmail.com>
2024-03-06 07:10:58 +0500

    fix: typing issues
    Signed-off-by: Anton Zimin <zimkaa87@gmail.com>

--- src/config/project_info.py
+++ src/config/project_info.py
@@ -1,13 +1,15 @@
+from pathlib import Path
+
 import tomllib
 
 
 def get_name() -> str:
-    with open("pyproject.toml", "rb") as f:  # noqa: PTH123
+    with Path("pyproject.toml").open("rb") as f:
         data = tomllib.load(f)
     return data["tool"]["poetry"]["name"]
 
 
 def get_version() -> str:
-    with open("pyproject.toml", "rb") as f:  # noqa: PTH123
+    with Path("pyproject.toml").open("rb") as f:
         data = tomllib.load(f)
     return data["tool"]["poetry"]["version"]
